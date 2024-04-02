616933f6ed7ecbd693d2139334f8300a81ea1a9f
husickova <156668585+husickova@users.noreply.github.com>
2024-04-02 23:40:58 +0200

    Update keboolabot2.py

--- keboolabot2.py
+++ keboolabot2.py
@@ -54,7 +54,7 @@ def create_conversational_chain(vector_store):
     # Create llm
     llm = LlamaCpp(
     streaming = True,
-    model_path= "/Users/katerinahusickova/Dokumenty/mistral-7b-instruct-v0.2.Q5_K_S.gguf",
+    model_path = st.file_uploader("Upload your MISTRAL model file", type=["gguf"]).name,
     temperature=0.75,
     top_p=1, 
     verbose=True,
